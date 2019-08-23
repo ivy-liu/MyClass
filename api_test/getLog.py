715e7e06ccf9fc8a4b88035e9069ea47b46c401b
lvy <ivy.liu.liuwanying@outlook.com>
2019-08-23 18:31:56 +0800

    Add files via upload

---
+++ api_test/getLog.py
@@ -0,0 +1,111 @@
+import logging
+from logging import handlers
+import time,os
+
+class Log:
+    level_relations = {
+        'debug':logging.DEBUG,
+        'info':logging.INFO,
+        'warning':logging.WARNING,
+        'error':logging.ERROR,
+        'crit':logging.CRITICAL
+    }#日志级别关系映射
+    
+    # when=D，新生成的文件名上会带上时间,
+    # 举个栗子，从今天执行到明天，会生成带有明天日期的文件，一天之内执行完，不会另外生成
+    def __init__(self,level='info',when='D',backCount=3,fmt='%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s'):
+        
+        t = time.strftime('%Y-%m-%d',time.localtime())  #将指定格式的当前时间以字符串输出
+        filename=t + '.log'
+        newfile='logs\\' + filename
+        if not os.path.exists(newfile):
+            f = open(newfile,'w') 
+            print ('文件创建 ',filename)
+            f.close()
+            print (newfile + " created.")
+        else:
+            print (newfile + " already existed.")
+
+
+        self.logger = logging.getLogger(newfile)
+        format_str = logging.Formatter(fmt)#设置日志格式
+        self.logger.setLevel(self.level_relations.get(level))#设置日志级别
+        sh = logging.StreamHandler()#往屏幕上输出
+        sh.setFormatter(format_str) #设置屏幕上显示的格式
+        #  logging.handlers.RotatingFileHandler -> 按照大小自动分割日志文件，一旦达到指定的大小重新生成文件
+        #  logging.handlers.TimedRotatingFileHandler  -> 按照时间自动分割日志文件 
+        # th = handlers.TimedRotatingFileHandler(filename=newfile,when='m',backupCount=backCount,encoding='utf-8')#往文件里写入#指定间隔时间自动生成文件的处理器
+        #实例化TimedRotatingFileHandler
+        #interval是时间间隔，backupCount是备份文件的个数，如果超过这个个数，就会自动删除，when是间隔的时间单位，单位有以下几种：
+        # S 秒
+        # M 分
+        # H 小时、
+        # D 天、
+        # W 每星期（interval==0时代表星期一）
+        # midnight 每天凌晨
+
+        th=logging.FileHandler(newfile,mode='w',encoding='utf-8')
+
+        th.setFormatter(format_str)#设置文件里写入的格式
+        self.logger.addHandler(sh) #把对象加到logger里
+        self.logger.addHandler(th)
+        # if __name__ == '__main__':
+        #     log = Logger('all.log',level='debug')
+        #     log.logger.debug('debug')
+        #     log.logger.info('info')
+        #     log.logger.warning('警告')
+        #     log.logger.error('报错')
+        #     log.logger.critical('严重')
+        #     Logger('error.log', level='error').logger.error('error')
+
+    def error(self,content):
+        self.logger.error(content)
+    def critical(self,content):
+        self.logger.critical(content)
+    def warning(self,content):
+        self.logger.warning(content)
+    def info(self,content):
+        self.logger.info(content)
+    # def debug(self,content):
+    #     self.logger.debug(content)
+
+# 日志级别： debug < info < warning < error < critical
+# logging.debug('debug级别，最低级别，一般开发人员用来打印一些调试信息')
+# logging.info('info级别，正常输出信息，一般用来打印一些正常的操作')
+# logging.warning('waring级别，一般用来打印警信息')
+# logging.error('error级别，一般用来打印一些错误信息')
+# logging.critical('critical 级别，一般用来打印一些致命的错误信息,等级最高')
+
+# #测试数据
+# log=Log()
+# log.error('hahhahaha11111')
+# time.sleep(10)
+# log.error('哈哈呵')
+
+
+
+
+# #创建一个logger
+# logger=logging.getLogger()
+# logger.setLevel(logging.INFO)
+
+# #创建一个handler，将log写入文件
+# fh=logging.FileHandler(log_path,mode='w',encoding='utf-8')
+# fh.setLevel(logging.INFO)
+
+# #再创建一个handler，将log输出到控制台
+# ch=logging.StreamHandler()
+# ch.setLevel(logging.INFO)
+
+# #设置输出格式
+# log_format="%(asctime)s %(filename)s [line:%(lineno)d] %(levelname)s: %(message)s"
+# formatter=logging.Formatter(log_format)
+# fh.setFormatter(formatter)
+# ch.setFormatter(formatter)
+
+# #把handler添加到logger里
+# logger.addHandler(fh)
+# logger.addHandler(ch)
+
+# logger.error(content)
+
--- api_test/getResponse.py
+++ api_test/getResponse.py
@@ -5,13 +5,14 @@ requests模块中，r.json()为Requests中内置的JSON解码器
 其中只有response返回为json格式时，用r.json()打印出响应的内容，
 如果response返回不为json格式，使用r.json()会报错
 
-请求成功，返回响应结果的json解码json_r，和响应结果r
-json_r用来内容判断
-r用来响应码判断
 例如
-if r.status_code==200:
+if status_code==200:
             self.assertEqual('successed', json_r['reason'], "是状态吗")
 '''
+# 备用{'content-Type':'application/x-www-form-urlencoded;charset=UTF-8'}
+# 备用{'Content-Type':'application/json;charset=utf-8'}
+
+#返回json状态的响应，响应状态码，响应时间
 
 class HttpRequestResponse:
     def get(self, url, params=None, headers=None):
@@ -19,34 +20,53 @@ class HttpRequestResponse:
         print('GET请求参数--', params)
         try:
             r = requests.get(url, params=params, headers=headers)
+            status_code=r.status_code
+            print('GET响应状态码--', status_code)
             json_r = r.json()
             print('GET响应结果--', json_r)
-            return json_r,r
+            time=r.elapsed.total_seconds()
+            print('GET响应时间--',time)
+            return json_r,status_code,time
         except Exception as e:
             print('GET请求报错--', e)
 
-    def post_form(self, url, data, headers=None):
+    def post_form(self, url, data, headers={'content-Type':'application/x-www-form-urlencoded'}):
         print('POST_FORM请求url--', url)
         print('POST_FORM请求参数--', data)
         try:
             r = requests.post(url, data=data, headers=headers)
+            status_code=r.status_code
+            print('POST_FORM响应状态码--', status_code)
             json_r = r.json()
             print('POST_FORM响应结果--', json_r)
-            return json_r,r
+            time=r.elapsed.total_seconds()
+            print('POST_FORM响应时间--',time)
+            return json_r,status_code,time
         except Exception as e:
             print('POST_FORM请求报错--', e)
 
-    def post_json(self, url, data, headers=None):
+    def post_json(self, url, data, headers={'Content-Type':'application/json'}):
         print('POST_JSON请求url--', url)
         print('POST_JSON请求参数--', data)
         try:
             r = requests.post(url, json=data, headers=headers)
+            status_code=r.status_code
+            print('POST_JSON响应状态码--', status_code)
             json_r = r.json()
             print('POST_JSON响应结果--', json_r)
-            return json_r,r
+            time=r.elapsed.total_seconds()
+            print('POST_JSON响应时间--',time)
+            return json_r,status_code,time
         except Exception as e:
             print('POST_JSON请求报错--', e)
+       
 
+
+
+
+
+
+            
 '''
 # get
 new_get = HttpRequestResponse()
@@ -55,8 +75,10 @@ params = {
     "bookname": "接口来自moco",
     "checkstatus": "on"
 }
-get_json = new_get.get(url, params=params)
+get_json,status_code,time = new_get.get(url, params=params)
 print("get_json---", get_json)
+print("GET响应状态码=", status_code)
+print("GET接口的响应时间=", time, '秒')
 
 # post_form
 newForm = HttpRequestResponse()
@@ -68,8 +90,10 @@ data = {
     "username": "xiaoming",
     "pwd": "123456"
 }
-form_json=newForm.post_form(url,data=data,headers=headers)
+form_json,status_code,time=newForm.post_form(url,data=data,headers=headers)
 print("form_json---",form_json)
+print("POST_FORM响应状态码=", status_code)
+print("POST_FORM接口的响应时间=", time, '秒')
 
 #post_json
 newJson = HttpRequestResponse()
@@ -81,6 +105,31 @@ data = {
     "username": "xiaoqiang",
     "pwd": "123456"
 }
-json_json=newJson.post_json(url,data,headers)
+json_json,status_code,time=newJson.post_json(url,data,headers)
 print("json_json---",json_json)
+print("POST_JSON响应状态码=", status_code)
+print("POST_JSON接口的响应时间=", time, '秒')
+'''
+
+
+
+'''
+ def test_m4(self):
+
+        # get
+        new_get = HttpRequestResponse()
+        url = 'http://localhost:12306/book_info'
+        params = {
+            "bookname": "接口来自moco",
+            "checkstatus": "on"
+        }
+        get_json,status_code,time= new_get.get(url, params=params)
+        print("get_json---", get_json)
+        if status_code==200:
+            self.assertEqual('successed', get_json['reason'], "是状态吗")
+            print('断言结束')
+        else:
+            print('断言结果不符合预期')
+
+
 '''
